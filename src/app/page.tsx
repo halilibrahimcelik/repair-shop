b25a29e687ba61645c58d5ed67eed322a59b9c48
halilibrahimcelik <halil.celik139@gmail.com>
2024-12-10 11:15:22 +0000

    feat:general typogpraphy added to the text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
+import { Card } from '@/components/ui/card';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className=' aspect-square object-cover bg-center bg-cover h-screen w-screen bg-home-img '>
       <main className='flex items-center justify-center w-full h-full'>
-        <div>
-          <h1>
+        <Card className='p-6 flex flex-col gap-2'>
+          <h1 className='heading'>
             {' '}
             Halil&apos;s <br /> Repair Shop{' '}
           </h1>
-          <address>
+          <address className='text-xl'>
             555 London Peckham Rd. <br />
             <p>SE1 5EU</p>
           </address>
-          <p>
-            Open Daily 9am to 9pm
-            <Link href='tel:5555555555' className='hover:underline'>
-              5555555555
+          <p className='text-xl'>
+            Open Daily 9am to 9pm <br />
+            <Link href='tel:55554443333' className='hover:underline'>
+              5555 444 333
             </Link>
           </p>
-        </div>
+        </Card>
       </main>
     </div>
   );
