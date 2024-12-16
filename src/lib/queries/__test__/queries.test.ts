e53d1d80c148d8a012c2e10cf20f1098be57204b
halilibrahimcelik <halil.celik139@gmail.com>
2024-12-16 14:55:08 +0000

    feat:added more test to getCostmer query.

--- src/lib/queries/__test__/queries.test.ts
+++ src/lib/queries/__test__/queries.test.ts
@@ -7,10 +7,25 @@ beforeEach(() => {
   fetchMock.resetMocks();
 });
 
-describe('check getCustomer function', () => {
-  it('should return customer with ID#1', async () => {
+describe('queries for database', () => {
+  it('should return customer with ID#1 via getCustomer', async () => {
     expect.assertions(1);
     const data = await getCustomer(1);
     expect(data?.firstName).toBe('John');
+  });
+  it('should return null when there is no customer on the table via getCustomer', async () => {
+    expect.assertions(1);
+
+    const data = await getCustomer(1231);
+    expect(data).toBeNull();
+  });
+  it('should captureError', async () => {
+    try {
+      await getCustomer(323);
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toBe(error);
+      }
+    }
   });
 });
